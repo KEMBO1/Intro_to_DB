60e0fb3481f43563eb42878c901cf5558b1f59a2
KAIBEI <anthokemboi6@gmail.com>
2025-07-03 09:40:01 +0300

    Clean final version with proper CREATE DATABASE syntax for ALX

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,58 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
---
--- Host: localhost    Database: alx_book_store
--- ------------------------------------------------------
--- Server version	8.0.41
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `alx_book_store`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `alx_book_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  `alx_book_store`;
 
 USE `alx_book_store`;
 
---
--- Table structure for table `authors`
---
 
 DROP TABLE IF EXISTS `AUTHORS`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `AUTHORS` (
   `AUTHOR_ID` int NOT NULL AUTO_INCREMENT,
   `AUTHOR_NAME` varchar(215) NOT NULL,
   PRIMARY KEY (`AUTHOR_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `authors`
---
 
 LOCK TABLES `authors` WRITE;
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `books`
---
-
 DROP TABLE IF EXISTS `BOOKS`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `BOOKS` (
   `BOOK_ID` int NOT NULL AUTO_INCREMENT,
   `TITLE` varchar(130) NOT NULL,
@@ -63,24 +26,12 @@ CREATE TABLE `BOOKS` (
   KEY `AUTHOR_ID` (`AUTHOR_ID`),
   CONSTRAINT `books_ibfk_1` FOREIGN KEY (`AUTHOR_ID`) REFERENCES `authors` (`AUTHOR_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `books`
---
 
 LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `customers`
---
 
 DROP TABLE IF EXISTS `CUSTOMERS`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `CUSTOMERS` (
   `CUSTOMER_ID` int NOT NULL AUTO_INCREMENT,
   `CUSTOMER_NAME` varchar(215) NOT NULL,
@@ -88,25 +39,14 @@ CREATE TABLE `CUSTOMERS` (
   `ADDRESS` text,
   PRIMARY KEY (`CUSTOMER_ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `customers`
---
+
 
 LOCK TABLES `customers` WRITE;
-/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
 INSERT INTO `customers` VALUES (1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.'),(2,'Blessing Malik','bmalik@sandtech.com','124 Happiness Ave.'),(3,'Obed Ehoneah','eobed@sandtech.com','125 Happiness Ave.'),(4,'Nehemial Kamolu','nkamolu@sandtech.com','126 Happiness Ave.');
-/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `order_details`
---
-
 DROP TABLE IF EXISTS `ORDER DETAILS`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ORDER DETAILS` (
   `ORDERDETAILID` int NOT NULL AUTO_INCREMENT,
   `ORDER_ID` int DEFAULT NULL,
@@ -118,24 +58,12 @@ CREATE TABLE `ORDER DETAILS` (
   CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`ORDER_ID`) REFERENCES `orders` (`ORDER_ID`),
   CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`BOOK_ID`) REFERENCES `books` (`BOOK_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `order_details`
---
 
 LOCK TABLES `order_details` WRITE;
-/*!40000 ALTER TABLE `order_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order_details` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `orders`
---
 
 DROP TABLE IF EXISTS `ORDERS`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ORDERS` (
   `ORDER_ID` int NOT NULL AUTO_INCREMENT,
   `CUSTOMER_ID` int DEFAULT NULL,
@@ -144,24 +72,6 @@ CREATE TABLE `ORDERS` (
   KEY `CUSTOMER_ID` (`CUSTOMER_ID`),
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CUSTOMER_ID`) REFERENCES `customers` (`CUSTOMER_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
 LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-07-03  9:16:36
